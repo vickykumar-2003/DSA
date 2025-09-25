0db8a819634b2310668998bae408a066549e783a
vickykumar-2003 <vickyk15009@gmail.com>
2025-09-25 12:06:37 +0530

    py

--- DSA/Array/1_D.py
+++ DSA/Array/1_D.py
@@ -7,15 +7,44 @@
 # for i in range(len(arr)):
 #     print(arr[i])
     
-# insert element in the array....
-print("how many element to store inside the aarry,",end="")
-num = input()
-arr=[]
-print("\n enter ",num,"elemnt:",end="")
-num = int(num)
-for i in range(num):
-    element = input()
-    arr.append(element)
-print("\n The array element are")
-for i in range(num):
-    print(arr[i],end="")
+# # insert,remove element in the 1-D array....
+# arr = [10,20,30,40,50]
+
+# print("Element at index 2:",arr[2]) # 30
+
+# for i in arr:
+#     print(i,end=" ") # 10.20,30,40,50
+
+
+# arr.append(60)
+# print("\n After append:",arr)# 10,20,30,40,50,60
+
+
+# arr.remove(30) # 10,20,40,50,60
+# print("Afetr remove:",arr)
+
+# 1-D Array using array module.....
+import array as arr 
+
+#integer element
+number = arr.array('i',[10,20,30,40,50]) # i ka mtlb h integr type
+
+# Access element
+print("element at index 1:",number[1])
+
+# Add element
+number.append(60)
+print("After append:",number)
+
+# Remove element
+number.remove(30)
+print("After remove:",number)
+
+
+
+
+
+
+
+
+
