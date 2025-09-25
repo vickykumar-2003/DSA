8b94b5d1e32581525a8d21f3572890f49b3fd68c
vickykumar-2003 <vickyk15009@gmail.com>
2025-09-25 14:55:06 +0530

    py

---
+++ DSA/Stack/implementation_using_deque.py
@@ -0,0 +1,14 @@
+# inlementation using deque...
+from collections import deque
+
+stack = deque()
+
+stack.append("vicky")
+print(stack)
+
+stack.append("kumar")
+stack.append("bihar")
+print(stack)
+print(stack.pop())
+print(stack)
+
---
+++ DSA/Stack/implementation_using_list.py
@@ -0,0 +1,9 @@
+# implementation using list...
+stack = []
+
+stack.append('welcome')
+stack.append('to')
+stack.append('lerning python')
+print(stack)
+print(stack.pop())
+print(stack)
