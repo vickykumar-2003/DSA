181f735f012a3a808973af24ead61885d9691c12
vickykumar-2003 <vickyk15009@gmail.com>
2025-10-07 09:48:07 +0530

    py

--- DSA/Stack/implementation_using_deque.py
+++ DSA/Stack/implementation_using_deque.py
@@ -1,14 +1,30 @@
-# inlementation using deque...
-from collections import deque
+# # inlementation using deque...
+# from collections import deque
 
-stack = deque()
+# stack = deque()
 
-stack.append("vicky")
-print(stack)
+# stack.append("vicky")
+# print(stack)
 
-stack.append("kumar")
-stack.append("bihar")
-print(stack)
-print(stack.pop())
-print(stack)
+# stack.append("kumar")
+# stack.append("bihar")
+# print(stack)
+# print(stack.pop())
+# print(stack)
 
+
+class Solution:
+   def isPalindrome(x: int) -> bool:
+    # negative numbers are not palindrome
+    if x < 0:
+        return False
+    # convert number to string and check
+    s = str(x)
+    return s == s[::-1]
+
+# Examples
+print(isPalindrome(121))   # True
+print(isPalindrome(-121))  # False
+print(isPalindrome(10))    # False
+
+           
